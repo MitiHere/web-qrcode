224e3aca9d93ef170a2c88c43333f0dd7d373342
EYRAUD Come <come.eyraud@telecom-paris.fr>
2023-09-29 17:17:35 +0200

    fixing things

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ app.use(
   })
 );
 
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/client.js", (req, res) => {
   res.sendFile(__dirname + "/public/client.js");
